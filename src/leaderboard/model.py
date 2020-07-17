2b1718c01ccabeb3ec6b049aae85313ad1d628c7
Michael Skarbek <mskarbek@redhat.com>
2020-07-16 21:24:33 -0400

    some refactoring

--- src/leaderboard/model.py
+++ src/leaderboard/model.py
@@ -33,34 +33,25 @@ def convert_opinion(agent_list, time):  # noqa: C901
     def calc_new_O(a):
         return a.od_opinion[time] - (a.od_opinion[time] - a.O_ave[time]) / a.F[time]
 
-    diff_max = get_diff()
-    high_thresh, low_thresh = get_threshold()
-
-    for gattr, sattr in {"score": "S_ave"}.items():
+    def calc_for_thresholds(gattr, sattr):
         for i, a in enumerate(agent_list):
             if a.rank[time] < high_thresh:
                 getattr(a, sattr).append(high_leader(i, gattr))
             elif low_thresh >= a.rank[time] >= high_thresh:
                 getattr(a, sattr).append(mid_leader(i, gattr))
-            elif a.rank[time] > low_thresh:
-                getattr(a, sattr).append(low_leader(i, gattr))
             else:
-                print("You done messed up")
+                getattr(a, sattr).append(low_leader(i, gattr))
+
+    diff_max = get_diff()
+    high_thresh, low_thresh = get_threshold()
+
+    calc_for_thresholds("score", "S_ave")
 
     for a in agent_list:
         a.D.append(abs(a.score[time] - a.S_ave[time]))
         a.F.append(calc_f(a, diff_max))
 
-    for gattr, sattr in {"od_opinion": "O_ave"}.items():
-        for i, a in enumerate(agent_list):
-            if a.rank[time] < high_thresh:
-                getattr(a, sattr).append(high_leader(i, gattr))
-            elif low_thresh >= a.rank[time] >= high_thresh:
-                getattr(a, sattr).append(mid_leader(i, gattr))
-            elif a.rank[time] > low_thresh:
-                getattr(a, sattr).append(low_leader(i, gattr))
-            else:
-                print("You done messed up")
+    calc_for_thresholds("od_opinion", "O_ave")
 
     for a in agent_list:
         a.new_opinion.append(calc_new_O(a))
