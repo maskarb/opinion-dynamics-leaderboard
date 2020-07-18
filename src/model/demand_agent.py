624f4333cb457f0f24cdd84df6ca6e6a8a0b34cb
Michael Skarbek <mskarbek@redhat.com>
2020-07-18 11:17:30 -0400

    make use of properties and setters

--- src/model/demand_agent.py
+++ src/model/demand_agent.py
@@ -8,9 +8,10 @@ class DemandAgent(Agent):
 
     def __init__(self, unique_id, model, leaderboard):
         super().__init__(unique_id, model)
+        self.id = unique_id
         self.type = leaderboard
-        self.od_opinion = [0]
-        self.decision = [np.random.randint(0, 2)]  # [0]
+        self._od_opinion = [0]
+        self._decision = [np.random.randint(0, 2)]  # [0]
         self.a = np.random.randint(0, 2, size=self.model.n_agents)
         self.b = np.random.randint(0, 2, size=self.model.n_agents)
         self.threshold = np.random.uniform(0.3, 0.9)  # 0.9
@@ -32,8 +33,44 @@ class DemandAgent(Agent):
         return self.od_opinion[self.time] > other.od_opinion[self.time]
 
     @property
+    def decision(self):
+        return self._decision[self.time]
+
+    @property
+    def last_decision(self):
+        return self._decision[-1]
+
+    @property
+    def last_od_opinion(self):
+        return self.od_opinion[-1]
+
+    @property
+    def od_opinion(self):
+        return self._od_opinion
+
+    @property
+    def opinion(self):
+        return getattr(self, self.type)[self.time]
+
+    @property
+    def sum_a(self):
+        return sum(self.a)
+
+    @property
+    def sum_b(self):
+        return sum(self.b)
+
+    @property
     def time(self):
         return self.model.schedule.time
+
+    @decision.setter
+    def decision(self, val):
+        self._decision.append(val)
+
+    @od_opinion.setter
+    def od_opinion(self, val):
+        self._od_opinion.append(val)
 
     def update_decision(self):
         # self.decision = 1
@@ -47,10 +84,10 @@ class DemandAgent(Agent):
         #     self.decision.append(0)
 
         # Du formulation
-        if self.decision[-1] == 0 and self.od_opinion[-1] < self.threshold:
-            self.decision.append(0)
+        if self.last_decision == 0 and self.last_od_opinion < self.threshold:
+            self.decision = 0
         else:
-            self.decision.append(1)
+            self.decision = 1
 
     def update_opinion(self):  # noqa: C901
         gb = self.model.global_broadcast  # 0 or 1
@@ -58,40 +95,40 @@ class DemandAgent(Agent):
 
         # Opinion from global_broadcast + social media:
         if self.model.scenario == 1:
-            O_j = getattr(self, self.type)[self.time]
+            O_j = self.opinion
             op_gb = (O_j + u_j * gb) / (1 + u_j)
-            self.od_opinion.append(op_gb)
+            self.od_opinion = op_gb
 
         # Opinion from global_broadcast + social media:
         elif self.model.scenario == 2:
-            sum_aijOi = self.a[self.unique_id] * getattr(self, self.type)[self.time]  # 0
+            sum_aijOi = self.a[self.id] * self.opinion  # 0
             for agent in self.model.schedule.agents:
-                if agent.unique_id != self.unique_id:
-                    sum_aijOi += self.a[agent.unique_id] * getattr(agent, agent.type)[self.time]
-            op_gb_sm = (sum_aijOi + u_j * gb) / (sum(self.a) + u_j)
-            self.od_opinion.append(op_gb_sm)
+                if agent.id != self.id:
+                    sum_aijOi += self.a[agent.id] * self.opinion
+            op_gb_sm = (sum_aijOi + u_j * gb) / (self.sum_a + u_j)
+            self.od_opinion = op_gb_sm
 
         # Opinion from global_broadcast + neighbors:
         elif self.model.scenario == 3:
-            O_j = getattr(self, self.type)[self.time]
-            sum_bijXi = self.b[self.unique_id] * self.decision[self.time]
+            O_j = self.opinion
+            sum_bijXi = self.b[self.id] * self.decision
             for agent in self.model.schedule.agents:
-                if agent.unique_id != self.unique_id:
-                    sum_bijXi += self.b[agent.unique_id] * agent.decision[self.time]
-            op_gb_n = (O_j + sum_bijXi + u_j * gb) / (1 + sum(self.b) + u_j)
-            self.od_opinion.append(op_gb_n)
+                if agent.id != self.id:
+                    sum_bijXi += self.b[agent.id] * agent.decision
+            op_gb_n = (O_j + sum_bijXi + u_j * gb) / (1 + self.sum_b + u_j)
+            self.od_opinion = op_gb_n
 
         # Opinion from global_broadcast + social media + neighbors:
         elif self.model.scenario == 4:
-            O_j = getattr(self, self.type)[self.time]
-            sum_aijOi = self.a[self.unique_id] * getattr(self, self.type)[self.time]
-            sum_bijXi = self.b[self.unique_id] * self.decision[self.time]
+            O_j = self.opinion
+            sum_aijOi = self.a[self.id] * self.opinion
+            sum_bijXi = self.b[self.id] * self.decision
             for agent in self.model.schedule.agents:
-                if agent.unique_id != self.unique_id:
-                    sum_aijOi += self.a[agent.unique_id] * getattr(agent, agent.type)[self.time]
-                    sum_bijXi += self.b[agent.unique_id] * agent.decision[self.time]
-            op_gb_sm_n = (sum_aijOi + sum_bijXi + u_j * gb) / (sum(self.a) + sum(self.b) + u_j)
-            self.od_opinion.append(op_gb_sm_n)
+                if agent.id != self.id:
+                    sum_aijOi += self.a[agent.id] * self.opinion
+                    sum_bijXi += self.b[agent.id] * agent.decision
+            op_gb_sm_n = (sum_aijOi + sum_bijXi + u_j * gb) / (self.sum_a + self.sum_b + u_j)
+            self.od_opinion = op_gb_sm_n
 
     def calculate_score(self):
         opinion = self.od_opinion[self.time]
